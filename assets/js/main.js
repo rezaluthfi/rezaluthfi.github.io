46312a90a9c6d0593d6193b5421c5effa9cbe4df
Your Name <you@example.com>
2022-12-29 13:13:00 +0700

    2st coomit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,4 @@
 //Scroll Sections Active Link
-// const sections = document.querySelectorAll('section[id]')
-
-// function scrollActive(){
-//     const scrollY = window.pageYOffset
-
-//     sections.forEach(current => {
-//         const sectionHeight = current.offsetHeight,
-//             sectionTop = current.offsetTop - 58,
-//             sectionId = current.getAttribute('id')
-
-//             if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-//                 document.querySelector('nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-//             } else {
-//                 document.querySelector('nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-//             }
-//     })
-// }
-// window.addEventListener('scroll', scrollActive)
-
 const sections = document.querySelectorAll('section[id]')
     
 const scrollActive = () =>{
@@ -40,13 +21,6 @@ window.addEventListener('scroll', scrollActive)
 
 
 
-//Scroll Top
-let scrollTop = document.querySelector(".scroll-top");
-
-window.addEventListener("scroll", () => {
-    scrollTop.classList.toggle("scroll-active", window.scrollY >= 400)
-});
-
 //Scroll Reveal Animation
 const sr = ScrollReveal ({
     origin: 'top',
@@ -61,3 +35,60 @@ sr.reveal('.home-text', {delay: 900})
 sr.reveal('.social', {delay: 1100, origin: 'bottom'})
 sr.reveal('.section__subtitle-skills, .section__title-skills', {delay: 2000})
 sr.reveal('.skills-content', {delay: 2000, origin: 'bottom'})
+
+
+
+//Email JS
+function validate() {
+	let name = document.querySelector('.name')
+	let email = document.querySelector('.email')
+	let msg = document.querySelector('.message')
+	let sendBtn = document.querySelector('.send-btn')
+
+	sendBtn.addEventListener('click', (e) => {
+		e.preventDefault();
+		if (name.value == "" || email.value == "" || msg.value == "") {
+			emptyerror();
+		} else {
+			sendmail (name.value, email.value, msg.value);
+			success();
+		}
+	})
+}
+validate();
+
+function sendmail(name, email, msg) {
+	emailjs.send("service_mfa515f","rezaluthfi_template",{
+		to_name: name,
+		from_name: email,
+		message: msg,
+		});
+}
+
+function emptyerror () {
+	swal({
+		title: "Error!",
+		text: "Fields cannot be empty!",
+		icon: "error",
+	  });
+}
+function success () {
+	swal({
+		title: "Success!",
+		text: "Thanks for contact me",
+		icon: "success",
+	  });
+}
+
+
+
+//Scroll Top
+let scrollTop = document.querySelector(".scroll-top");
+
+window.addEventListener("scroll", () => {
+    scrollTop.classList.toggle("scroll-active", window.scrollY >= 400)
+});
+
+
+
+
